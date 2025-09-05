b25b3ebd9ea42d25935f8c503e65799f52c783fc
liaakin <lia.07@live.de>
2025-09-05 17:53:55 +0200

    Footer Text

--- app/_components/Footer.js
+++ app/_components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="absolute bottom-6 overflow-hidden text-center w-full text-xs sm:text-sm tracking-widest text-transparent bg-[#666666] [background-clip:text] [text-shadow:0px_3px_3px_rgba(255,255,255,0.5)]">
-      © 2025 Liascope Astrology | Developed & Created with ✨ by Liascope. All rights reserved.
+    <footer className="absolute bottom-4 overflow-hidden text-center w-full text-xs sm:text-sm tracking-widest text-transparent bg-[#666666] [background-clip:text] [text-shadow:0px_1px_1px_rgba(255,255,255,0.5)]">
+      © 2025 Liascope Astrology | Developed & Created with ✨ All rights reserved.
     </footer>
   );
 }
