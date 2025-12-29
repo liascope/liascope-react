c95e8a38436924da8d43a7a25425247faf7c50cf
liaakin <lia.07@live.de>
2025-12-29 20:45:33 +0100

    refactor: rewrite footer

--- app/_components/Footer.js
+++ app/_components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="my-10 overflow-hidden text-center w-full text-xs sm:text-sm tracking-widest drop-shadow-xl text-stone-500/70">
-      © 2025 Liascope Astrology | Developed & Created with ✨ All rights reserved.
+     Liascope Astrology | © {new Date().getFullYear()} Liascope
     </footer>
   );
 }
